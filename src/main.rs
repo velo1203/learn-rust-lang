0af8b584b5c51034d8dfba9a9304be9f816d296d
velo1203 <hokitoki1203@gmail.com>
2024-07-24 13:48:04 +0900

    learn function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    let tup : (i32,bool) = (500,true);
-    println!("{}",tup.0);
-
+    let a: i32 = another_function(1);
+    println!("{}",a);
 }
+
+fn another_function(x:i32) ->i32 {
+    println!("{} Another Function!",x);
+    return  x + 10; //세미 콜론 없이 반환할 값을 명시 할 수 있다
+}   
