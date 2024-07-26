22dbdaea15d05040687a7dddad8cdeca2bdf146d
velo1203 <hokitoki1203@gmail.com>
2024-07-26 14:54:46 +0900

    learn vec

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
 fn main(){
-    let number = Some(30);
-    let number_plus = plus_one(number);
     
-}
-
-fn plus_one(number:Option<i32>) -> Option<i32>{
-    match number {
-        Some(i) => Some(i+1),
-        None => None
-    }
 }
